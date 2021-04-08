af5a514a4b99ae2b0e4933aa264c9ec495a17da7
FawwazMayda <33770567+FawwazMayda@users.noreply.github.com>
2021-04-08 22:31:10 +0800

    initial add

---
+++ rlMLpack/qlearning.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <mlpack/core.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/init_rules/gaussian_init.hpp>
+#include <mlpack/methods/reinforcement_learning/q_learning.hpp>
+#include <mlpack/methods/reinforcement_learning/environment/cart_pole.hpp>
+#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
+#include <mlpack/methods/reinforcement_learning/replay/random_replay.hpp>
+#include <ensmallen.hpp>
+
+using namespace std;
+using namespace mlpack;
+using namespace mlpack::ann;
+using namespace mlpack::rl;
+using namespace ens;
+
+int main() {
+
+    return 0;
+}
---
+++ rlMLpack/startrl.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mlpack/core.hpp>
+#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
+
+using namespace std;
+using namespace mlpack;
+int main() {
+
+    
+    return 0;
+}
